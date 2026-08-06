deba2ce304ad8f593f7c130a6bedd772b73b759d
tslacc <46695088+tslacc@users.noreply.github.com>
2026-08-06 16:51:01 -0400

    Organize structs
    Declare Database, Table, and Record structs
    Template for "instantiating" a new struct

--- lib_database.c
+++ lib_database.c
@@ -1,5 +1,11 @@
 #include "lib_database.h"
-struct Record *lookup() {
+
+struct Database *new_db(){
+	struct Database *result = malloc(sizeof(struct Database));
+	result->num.tables = 0;
+	return result;
+}
+struct Table *lookup(const struct Database, const char *_name){
 	return NULL;
 }
 void add(char *_name, void *value){
--- lib_database.h
+++ lib_database.h
@@ -1,6 +1,21 @@
+#ifndef LIB_DATABASE
+#define LIB_DATABASE
+
+#include <stddef.h>
+#include <stdlib.h>
 //Basic database containing a list of struct Record.
 //Each record contains exactly one name and one value.
 
+struct Database{
+	char *path;
+	struct Table *tables;
+	int num_tables;
+};
+struct Table{
+	char *name;
+	struct Record *entries;
+	int num_entries;
+};
 struct Record{
 	char *name;
 	union{
@@ -9,6 +24,11 @@ struct Record{
 		char as_char[sizeof(float)];
 	} value;
 };
+struct Database *new_db();
+struct Table *db_lookup_table(const struct Database db, const char *_name);
+struct Record *table_lookup_record(const struct Table tb, const char *_name);
+struct Table *db_new_table(struct Database *db, const char *_name);
+struct Record *table_new_record(struct Table *tb, const char *_name, void *value);
+void debug_print();
 
-void lookup();
-void add(char *_name, void *value);
+#endif
