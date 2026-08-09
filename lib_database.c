e47e8250cbf13e6d4a399a8cf16aa15bcf60521c
tslacc <46695088+tslacc@users.noreply.github.com>
2026-08-08 20:47:27 -0400

    Templating
    Added todo for planning
    Table/Records arrays may either add an entry to the tail of the list IF the current space is full, or if there is a deallocated space inside the array,

--- lib_database.c
+++ lib_database.c
@@ -1,10 +1,35 @@
 #include "lib_database.h"
 
+const int RESIZE_SCALE = 2;
 struct Database *new_db(){
 	struct Database *result = malloc(sizeof(struct Database));
 	result->num_tables = 0;
+	result->max_tables = 1;
 	return result;
 }
+//For reuse purposes, Lookup will return null IFF there is no de-allocated table in this database.
+//If there is a deallocated table, Lookup may return that pointer but pointing to null: if so, create a new table in that location.
+struct Table *database_new_table(struct Database *db, const char *_name){
+	struct Table *result = database_lookup_table(db, _name);
+	if(result!=NULL){
+		return result;
+	}
+	result = malloc(sizeof(struct Table *));
+	if(db->max_tables <= db->num_tables){
+		db->max_tables*=RESIZE_SCALE;
+		db->tables=realloc(db->tables,(sizeof(struct Table)*db->max_tables));
+	}
+	db->tables[db->num_tables]=result;
+	//TODO Change if this new table fills a hole instead;
+	db->num_tables++;
+	return result;
+}
+//TODO Implement
+struct Table *database_lookup_table(const struct Database *db, const char *_name){
+	return NULL;
+}
+
+
 struct Record *table_new_record(struct Table *tb, const char *_name, void *value){
 	struct Record *result = table_lookup_record(tb, _name);
 	if(result != NULL){
@@ -12,7 +37,7 @@ struct Record *table_new_record(struct Table *tb, const char *_name, void *value
 	}
 	result = malloc(sizeof(struct Record *));
 	if(tb->max_records <= tb->num_records){
-		tb->max_records*=2;
+		tb->max_records*=RESIZE_SCALE;
 		tb->records=realloc(tb->records,(sizeof(struct Record)*tb->max_records));
 
 	}
--- lib_database.h
+++ lib_database.h
@@ -8,8 +8,9 @@
 
 struct Database{
 	char *path;
-	struct Table *tables;
+	struct Table **tables;
 	int num_tables;
+	int max_tables;
 };
 struct Table{
 	char *name;
@@ -26,10 +27,12 @@ struct Record{
 	} value;
 };
 struct Database *new_db();
-struct Table *database_lookup_table(const struct Database *db, const char *_name);
-struct Record *table_lookup_record(const struct Table *tb, const char *_name);
 struct Table *database_new_table(struct Database *db, const char *_name);
+struct Table *database_lookup_table(const struct Database *db, const char *_name);
+
 struct Record *table_new_record(struct Table *tb, const char *_name, void *value);
+struct Record *table_lookup_record(const struct Table *tb, const char *_name);
+
 void debug_print();
 
 #endif
