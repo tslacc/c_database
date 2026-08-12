16a57dde38176199dd8f06837a5d4740f188ecbb
tslacc <46695088+tslacc@users.noreply.github.com>
2026-08-12 10:44:07 -0400

    Removed unique record/table limit
    Added internal search function for looking up blank entries in tables and dbs.

--- lib_database.c
+++ lib_database.c
@@ -5,32 +5,42 @@
 
 const int RESIZE_SCALE = 2;
 
-//TODO Implement
-//Modification: now returns the integer offset of the pointer to EITHER a complete match (Table exists and name matches) or a blank table (Table doesn't exist but resize isn't needed)
-static int database_lookup_table(const struct Database *db, const char *_name){
-	return -1;
+//RECORDS	==========================================================================================================
+//Allocate a new record.
+static struct Record *new_record(const char *name){
+	struct Record *result = malloc(sizeof(struct Record));
+	memcpy(result->name, name, strlen(name));
+	result->value.as_int = 0;
+	return result;
 }
 
-//Either looks up the matching table or allocates a new table in the db matching the name..
-static struct Table *database_lookup_or_new_table(struct Database *db, const char *_name){
-	int index = database_lookup_table(db, _name);
-	if(index!=-1 && db->tables[index] != NULL){ //A table already exists
-		return db->tables[index];
-	} else if (index!=-1 && db->tables[index] == NULL) { //A table doesn't already exist, but *result points to NULL and therefore can be changed to point to a new struct.
-		db->tables[index] = malloc(sizeof(struct Table));
-		db->num_tables++;
-		return db->tables[index];
-	} else { //No table exists: resize and allocate.
-		if(db->max_tables <= db->num_tables){
-			db->max_tables*=RESIZE_SCALE;
-			db->tables=realloc(db->tables,(sizeof(struct Table)*db->max_tables));
-		}
-		db->tables[db->num_tables] = malloc(sizeof(struct Table));
-		db->num_tables++;
-		return db->tables[db->num_tables-1];
-	}
-		
+//TABLES	=========================================================================================================
+//Searches for an empty slot to enter a new record or returns -1 if non exists.
+static int table_search_blank(const struct Table *tb){
+	return -1;
 }
+//TODO change to string-based parsing
+//Lookup a single record by name and return the first value.
+static struct Record *table_lookup_record(const struct Table *tb, const char *_name){
+	return NULL;
+}
+//Adds a new record to the table.
+struct Record *table_new_record(struct Table *tb, const char *_name){
+	int index = table_search_blank(tb);
+	struct Record *result = new_record(_name);
+	if(index == -1){ //No space exists. Resize and allocate
+		tb->max_records*=RESIZE_SCALE;
+		tb->records=realloc(tb->records,(sizeof(struct Record)*tb->max_records));
+		tb->records[tb->num_records] = result;
+	} else {//index != -1 and a valid space exists.
+		tb->records[index] = result;
+	}
+	tb->num_records++;
+	return result;
+	
+}
+
+//Allocate a new table.
 static struct Table *new_table(const char *name){
 	struct Table *result = malloc(sizeof(struct Table));
 	result->max_records = result->num_records = 1;
@@ -38,44 +48,37 @@ static struct Table *new_table(const char *name){
 	result->lookup_record_index = NULL;
 	return result;
 }
-static struct Record *new_record(const char *name){
-	struct Record *result = malloc(sizeof(struct Record));
-	memcpy(result->name, name, strlen(name));
-	result->value.as_int = 0;
+
+//DATABASES =============================================================================================================================================================================================
+static int database_search_blank(const struct Database *db){
+	return -1;
+}
+static int database_lookup_table(const struct Database *db, const char *_name){
+	return -1;
+}
+//Either looks up the matching table or allocates a new table in the db matching the name..
+static struct Table *database_new_table(struct Database *db, const char *_name){
+	int index = database_lookup_table(db, _name);
+	struct Table *result = new_table(_name);
+	if(index==-1){//No space exists. Expand and allocate
+		db->max_tables*=RESIZE_SCALE;
+		db->tables=realloc(db->tables,(sizeof(struct Table)*db->max_tables));
+		db->tables[db->num_tables] = result;
+	} else { //num tables is ok
+		db->tables[index] = result;
+	}
+	db->num_tables++;
 	return result;
 }
+
 struct Database *new_db(){
 	struct Database *result = malloc(sizeof(struct Database));
 	result->num_tables = 0;
 	result->max_tables = 1;
-	result->lookup_or_new_table = database_lookup_or_new_table;
+	result->lookup_or_new_table = database_new_table;
 	result->lookup_table_index = database_lookup_table;
 	return result;
 }
-//TODO finish implementing logic
-struct Record *table_new_record(struct Table *tb, const char *_name, void *value){
-	int index = tb->lookup_record_index(tb, _name);
-	
-	if(index == -1){ //no record exists and no space exists, realloc
-		return NULL;
-	} else if(index != -1 && tb->records[index]==NULL){ //No record exists but space exists. Allocate object
-		
-	} else { //Matching record already exists. Return this struct.
-		return tb->records[index];
-	}
-	struct Record *result = malloc(sizeof(struct Record *));
-	if(tb->max_records <= tb->num_records){
-		tb->max_records*=RESIZE_SCALE;
-		tb->records=realloc(tb->records,(sizeof(struct Record)*tb->max_records));
 
-	}
-	tb->records[tb->num_records] = result;
-	tb->num_records++;
-	return result;
-}
-struct Record *table_lookup_record(const struct Table *tb, const char *_name){
-	return NULL;
-}
-void add(char *_name, void *value){
-	return;
-}
+
+
