40c25bfee9bf84fd268bac7a99963e2ee95b95e7
tslacc <46695088+tslacc@users.noreply.github.com>
2026-08-20 20:08:24 -0400

    manual stdin boundscheck
    Swapped fgets to getc to prevent excess user input from breaking input

--- main.c
+++ main.c
@@ -11,8 +11,24 @@ struct Database *d;
 //Get single int
 int l_getint(){
 	char buf[64];
-	fgets(buf, 33, stdin);
-	if(errno!=0) while(getchar()!='\n'); //clear stdin
+	//Read at most 33 chars
+	int i = 0; 
+	int c = 0;
+	while(i<32 && (c=getc(stdin))!='\n'){
+		//printf("insert %d at %u\n", c, i);
+		buf[i] = c;
+		i++;
+	}
+	//printf("Not inserting char %d\n",c);
+	if(c!='\n') {
+		//printf("no endline reached. flush buffer\n");
+		while((c=getc(stdin))!='\n'){
+		//	printf("Read and discard c %d\n",c);
+		}
+		//printf("last c read is %d\n",c);
+		return -1;
+	}
+	buf[i]='\0';
 	return atoi(buf);
 }
 void table_loop(struct Table *tb){
